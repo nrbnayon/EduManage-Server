1fb46576d26644c0cc0cb60b157f76dc910906f0
nrbnayon <nrbnayon@gmail.com>
2024-06-08 16:46:35 +0600

    Create teacher request api

--- index.js
+++ index.js
@@ -36,6 +36,12 @@ async function run() {
     const courseCollection = client.db("EduManage").collection("allCourses");
     const feedbacksCollection = client.db("EduManage").collection("feedbacks");
     const statsCollection = client.db("EduManage").collection("stats");
+    const teacherCollection = client
+      .db("EduManage")
+      .collection("TeacherRequest");
+    const courseEnrollCollection = client
+      .db("EduManage")
+      .collection("EnrollCollection");
 
     //JWT
     app.post("/jwt", async (req, res) => {
@@ -70,7 +76,7 @@ async function run() {
       const email = req.decoded.email;
       const query = { email: email };
       const user = await userCollection.findOne(query);
-      const isAdmin = user?.role === "admin";
+      const isAdmin = user?.userRole === "admin";
       if (!isAdmin) {
         return res.status(403).send({ message: "forbidden access" });
       }
@@ -119,6 +125,69 @@ async function run() {
       }
     });
 
+    // Enroll Collection api
+
+    app.post("/enroll-course", async (req, res) => {
+      const course = req.body;
+      try {
+        const result = await courseEnrollCollection.insertOne(course);
+        res.send(result);
+      } catch (error) {
+        console.error("Error enroll:", error);
+      }
+    });
+    // single user enroll info
+    app.get("/enroll-info", async (req, res) => {
+      const email = req.query.studentEmail;
+      const query = { studentEmail: email };
+      try {
+        const result = await courseEnrollCollection.find(query).toArray();
+        res.send(result);
+      } catch (error) {
+        res.status(500).send("Failed to fetch Course ");
+      }
+    });
+
+    // total enrollment api
+
+    app.patch("/updateTotalEnrollment/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+
+      try {
+        // Find the course
+        const course = await courseCollection.findOne(filter);
+        if (!course) {
+          return res.status(404).send("Course not found");
+        }
+
+        // Update the totalEnrollment for the specific course
+        const updateCourseEnrollment = await courseCollection.updateOne(
+          filter,
+          {
+            $inc: { totalEnrollment: 1 },
+            // upsert: true,
+          }
+        );
+
+        // Check if the course enrollment update was successful
+        if (updateCourseEnrollment.modifiedCount === 0) {
+          return res.status(500).send("Failed to update course enrollment");
+        }
+
+        // Update the totalEnrollments in the stats collection
+        const updateTotalEnrollment = await statsCollection.updateOne(
+          { _id: new ObjectId("665ce46ccce6c97b84e3c1a4") },
+          { $inc: { totalEnrollments: 1 } },
+          { upsert: true }
+        );
+
+        res.send(updateTotalEnrollment);
+      } catch (error) {
+        console.error(error);
+        res.status(500).send("Failed to update total enrollment student");
+      }
+    });
     //create admin api
     app.patch("/users/admin/:id", verifyToken, async (req, res) => {
       const id = req.params.id;
@@ -135,19 +204,18 @@ async function run() {
     app.get("/users/admin/:email", verifyToken, async (req, res) => {
       try {
         const email = req.params.email;
-
         // Check if the email in the request matches the email in the token
         if (email !== req.decoded.email) {
           return res.status(403).send({ message: "Unauthorized access" });
         }
 
         // Query the database for the user by email
-        const query = { email: email };
+        const query = { userEmail: email };
         const user = await userCollection.findOne(query);
 
         let admin = false;
         if (user) {
-          admin = user.role === "admin";
+          admin = user.userRole === "admin";
         }
 
         // Send a JSON response
@@ -156,6 +224,28 @@ async function run() {
         res
           .status(500)
           .send({ message: "Internal server error", error: error.message });
+      }
+    });
+
+    //Teacher api
+    app.post("/teaching-request", async (req, res) => {
+      const request = req.body;
+      try {
+        const result = await teacherCollection.insertOne(request);
+        res.send(result);
+      } catch (error) {
+        console.error("Error enroll:", error);
+        res.status(500).send({ error: "Failed to submit teaching request" });
+      }
+    });
+
+    app.get("/teaching-request", async (req, res) => {
+      try {
+        const result = await teacherCollection.find().toArray();
+        res.send(result);
+      } catch (error) {
+        console.error("Error loading teaching requests:", error);
+        res.status(500).send({ error: "Failed to load teaching requests" });
       }
     });
 
